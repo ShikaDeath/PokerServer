352b71bd5bc542b162f7594d83fa2a9397b93c7f
Timur Rassolov <timur.rassolov@gmail.com>
2017-03-08 22:04:39 +0200

    pom changes: apache common libraries (lang, collections) test changes: tests to check these libraries

--- src/test/java/pokerproject/server/main/TempTest.java
+++ src/test/java/pokerproject/server/main/TempTest.java
@@ -1,10 +1,17 @@
 package pokerproject.server.main;
 
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 /**
  * Created by timurrassolov on 06/03/2017.
+ * <p>
+ * Some tests to show library usage
  */
 public class TempTest {
 
@@ -12,4 +19,20 @@ public class TempTest {
     public void testOk() {
         Assert.assertTrue(true);
     }
+
+    @Test
+    public void apacheCommonsLang() {
+        Assert.assertTrue(ObjectUtils.notEqual(null, "word"));
+        Assert.assertTrue(StringUtils.isEmpty(""));
+        Assert.assertTrue(StringUtils.isEmpty(null));
+
+    }
+
+    @Test
+    public void apacheCommmonsCollections() {
+        Assert.assertTrue(CollectionUtils.isEmpty(null));
+        Assert.assertTrue(CollectionUtils.isEmpty(new ArrayList()));
+    }
+
+
 }
