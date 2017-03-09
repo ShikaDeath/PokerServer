3c76c9c5efe06cf8be5f38fde49cd90c23b0c996
Timur Rassolov <timur.rassolov@gmail.com>
2017-03-09 21:50:29 +0200

    logging

--- src/main/java/pokerproject/server/main/Main.java
+++ src/main/java/pokerproject/server/main/Main.java
@@ -1,11 +1,17 @@
 package pokerproject.server.main;
 
+
+import org.apache.log4j.Logger;
+
 /**
  * Created by timurrassolov on 06/03/2017.
  */
 public class Main {
 
-    public static void main (String [] args){
-        System.out.println("Hello, World!");
+    final static Logger logger = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+        logger.info("Hello, World!");
     }
 }
